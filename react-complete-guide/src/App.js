82dd8c3b3fae134c0e4c46fb7c01eadac6c1f4ce
VinothKumar6595 <vinothcassillas@gmail.com>
2023-06-26 13:23:41 +0530

    Keys

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,43 +2,46 @@ import React, { useState } from "react";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import NewExpense from "./components/Expenses/NewExpense";
 import ExpenseFilter from "./components/Expenses/ExpenseFilter";
+import Card from "./components/UI/Card";
+
+const DUMMYEXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    location: "Chennai",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    location: "Banglore",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    location: "Mumbai",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location: "Delhi",
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: "Chennai",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      location: "Banglore",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: "Mumbai",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: "Delhi",
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMYEXPENSES);
 
   const addExpenseHandler = (newExpense) => {
-    console.log("in App.js");
-    expenses.push(newExpense);
-    console.log(expenses);
+    setExpenses((prevExpenses) => {
+      return [newExpense, ...prevExpenses];
+    });
   };
 
   const [filteredYear, setFilteredYear] = useState("");
@@ -47,7 +50,7 @@ const App = () => {
   };
 
   return (
-    <div>
+    <Card>
       <NewExpense onAddExpense={addExpenseHandler} />
       <ExpenseFilter
         selected={filteredYear}
@@ -59,10 +62,10 @@ const App = () => {
           amount={expense.amount}
           date={expense.date}
           location={expense.location}
-          id={expense.id}
+          key={expense.id}
         ></ExpenseItem>
       ))}
-    </div>
+    </Card>
   );
 };
 
